2af533650a998b92d2fb6a76aab1b5e2589bbb20
Simon Muriu <muriusimon6@gmail.com>
2025-07-07 11:57:14 +0300

    Cleaned up repo: removed node_modules from staging, added .gitignore

--- server/server.js
+++ server/server.js
@@ -6,9 +6,13 @@ const { Server } = require('socket.io');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
+const connectDB = require('./config/db'); 
 
 // Load environment variables
 dotenv.config();
+
+// Connect to MongoDB
+connectDB();
 
 // Initialize Express app
 const app = express();
@@ -124,7 +128,7 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
